10274150de0793ed7b1d44c0124668097f9ef425
hat-laptop <flint9877@gmail.com>
2022-08-22 12:50:47 +0800

    New import system

--- __main__.py
+++ __main__.py
@@ -1,43 +1,53 @@
 import os
-import platform
-
-SysOS = platform.system() #Grabs OS type
-
-from pathlib import Path
+import sys
 
 
 Developer_Mode = True #Dev stats
 
 
+import subprocess
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install("uni-curses")
+install("pyogg")
+
+install("PyOpenAL")
+install("pynput")
+install("pathlib")
+
+import platform
+SysOS = platform.system() #Grabs OS type
+
+from pathlib import Path
+
 working_directory = Path(__file__).absolute().parent #Grabs directory
 
-try:
-  f = open("save-file.txt", "x")
-  f.write("ImportProcess = Complete")
-  import subprocess
-  from subprocess import Popen
-  if SysOS == 'Windows':
-    p = Popen(f"{working_directory}/initboot.bat", shell=True, stdout = subprocess.PIPE) # Auto-installs required dependancies through initboot.bat
-    stdout, stderr = p.communicate()
-  else:
-    pass
-  f.close
+# try:
+#   f = open("save-file.txt", "x")
+#   f.write("ImportProcess = Complete")
+#   import subprocess
+#   from subprocess import Popen
+#   if SysOS == 'Windows':
+#     p = Popen(f"{working_directory}/initboot.bat", shell=True, stdout = subprocess.PIPE) # Auto-installs required dependancies through initboot.bat
+#     stdout, stderr = p.communicate()
+#   else:
+#     pass
+#   f.close
 
-except:
-  print("Import process has run before\nIf the game is not running, ignore this message.\nIf it's not running and you wish to retry imports, delete save-file.txt")
-  pass
-
-if not SysOS == 'Windows':
-    print("NOTICE: Not supported on platforms other then Windows.")
-    quit()
-  
+# except:
+#   print("Import process has run before\nIf the game is not running, ignore this message.\nIf it's not running and you wish to retry imports, delete save-file.txt")
+#   pass
 
 if Developer_Mode:
   print("Init boot complete")
 
-import keyboard
+from pynput.keyboard import Key, Controller
 
-keyboard.press_and_release("f11") #Makes the game full-screen before assigning xy dimentions to Curses
+keyboard = Controller()
+
+keyboard.press(Key.f11)
+keyboard.release(Key.f11)
 
 '''
 If you wish to see the original prototype of the game, enable this file.
