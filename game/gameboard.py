f548a5ad57c7f9af509ee1460e8ed29f510bd8ff
hat <flint9877@gmail.com>
2022-08-24 22:19:15 +0800

    changes to blank out card + sounds

--- game/gameboard.py
+++ game/gameboard.py
@@ -15,25 +15,32 @@ def startBoard():
         cardCentering = cardCentringOriginal
         soundPosition = soundPositionOriginal
         for card in range(0,4):
-            mvaddstr(sh // 2 + cardHeight, sw // 2 + cardCentering, test[0])
-            mvaddstr(sh // 2 + cardHeight +1, sw // 2 + cardCentering, test[1])
-            mvaddstr(sh // 2 + cardHeight +2, sw // 2 + cardCentering, test[2])
-            mvaddstr(sh // 2 + cardHeight +3, sw // 2 + cardCentering, test[3])
-            mvaddstr(sh // 2 + cardHeight +4, sw // 2 + cardCentering, test[4])
-            mvaddstr(sh // 2 + cardHeight +5, sw // 2 + cardCentering, test[5])
-            mvaddstr(sh // 2 + cardHeight +6, sw // 2 + cardCentering, test[6])
-            mvaddstr(sh // 2 + cardHeight +7, sw // 2 + cardCentering, test[7])
-            mvaddstr(sh // 2 + cardHeight +8, sw // 2 + cardCentering, test[8])
-            mvaddstr(sh // 2 + cardHeight +9, sw // 2 + cardCentering, test[9])
-            mvaddstr(sh // 2 + cardHeight +10, sw // 2 + cardCentering, test[10])
-            mvaddstr(sh // 2 + cardHeight +11, sw // 2 + cardCentering, test[11])
+            mvaddstr(sh // 2 + cardHeight, sw // 2 + cardCentering, blankCardSpace[0])
+            heightAddition = 1
+            for line in range(0,10):
+                mvaddstr(sh // 2 + cardHeight + heightAddition, sw // 2 + cardCentering, blankCardSpace[1])
+                heightAddition += 1
+            heightAddition = 0
+            mvaddstr(sh // 2 + cardHeight +11, sw // 2 + cardCentering, blankCardSpace[11])
             cardCentering += 20
-            CardPlaySound("quick",(soundPosition, 0,1))
+            CardPlaySound("glow",(soundPosition, 0,1))
             soundPosition += 0.1
-            sleep(0.3)
+            sleep(0.05)
         cardHeight += 12
 
-        
+
+# mvaddstr(sh // 2 + cardHeight, sw // 2 + cardCentering, test[0])
+# mvaddstr(sh // 2 + cardHeight + 1, sw // 2 + cardCentering, test[1])
+# mvaddstr(sh // 2 + cardHeight + 2, sw // 2 + cardCentering, test[2])
+# mvaddstr(sh // 2 + cardHeight + 3, sw // 2 + cardCentering, test[3])
+# mvaddstr(sh // 2 + cardHeight + 4, sw // 2 + cardCentering, test[4])
+# mvaddstr(sh // 2 + cardHeight + 5, sw // 2 + cardCentering, test[5])
+# mvaddstr(sh // 2 + cardHeight + 6, sw // 2 + cardCentering, test[6])
+# mvaddstr(sh // 2 + cardHeight + 7, sw // 2 + cardCentering, test[7])
+# mvaddstr(sh // 2 + cardHeight + 8, sw // 2 + cardCentering, test[8])
+# mvaddstr(sh // 2 + cardHeight + 9, sw // 2 + cardCentering, test[9])
+# mvaddstr(sh // 2 + cardHeight + 10, sw // 2 + cardCentering, test[10])
+# mvaddstr(sh // 2 + cardHeight + 11, sw // 2 + cardCentering, test[11])
 
 
 def CardPlaySound(tone="normal", position=(0,0,0)):
@@ -52,10 +59,18 @@ def CardPlaySound(tone="normal", position=(0,0,0)):
         ]
         PlaySound(f"mono/card/{random.choice(normal)}", 0.5, position)
     if tone == "quick":
-        normal = [
+        quick = [
             "cardquick#1",
             "cardquick#2",
             "cardquick#3",
             "cardquick#4"
         ]
-        PlaySound(f"mono/card/{random.choice(normal)}", 0.5, position)
+        PlaySound(f"mono/card/{random.choice(quick)}", 0.5, position)
+    if tone == "glow":
+        glow = [
+            "cardslot_glow#1",
+            "cardslot_glow#2",
+            "cardslot_glow#3",
+            "cardslot_glow#4"
+        ]
+        PlaySound(f"mono/card/{random.choice(glow)}", 0.5, position)
